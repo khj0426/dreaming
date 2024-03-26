5dfad8d835df62ea02992dd6a2b1617a12076b7b
Kim0426 <706shin1728@naver.com>
2024-03-27 01:00:04 +0900

    chore: 경로 변경

--- app/api/best-posts/route.ts
+++ app/api/best-posts/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '../../../../../prisma/client';
+import prisma from '../../../prisma/client';
 
 export async function GET(req: NextRequest) {
   const getAllBestPosts = await prisma.diary.findMany({
