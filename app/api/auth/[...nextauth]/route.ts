25624b558130aa0b3886d76b95166b53ee1a77ef
Kim0426 <706shin1728@naver.com>
2024-03-30 18:43:21 +0900

    refactor: 카카오 새 토큰 발급 받는 로직 분리

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 //app/api/auth/[...nextauth]/route.ts
 
+import { refreshKakaoAccessToken } from '../../../lib/oauth';
 import NextAuth from 'next-auth/next';
 import KakaoProvider from 'next-auth/providers/kakao';
 import { createNewUser } from '../../../lib/user';
@@ -15,36 +16,6 @@ interface KakaoUser extends DefaultUser {
     email: string;
   };
 }
-
-//카카오의 refresh토큰으로 새 accessToken을 받아온다.
-const refreshAccessToken = async (token: any) => {
-  try {
-    const response = await fetch('https://kauth.kakao.com/oauth/token', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      method: 'POST',
-      body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        client_id: `${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}`,
-        refresh_token: token.refreshToken,
-        client_secret: `${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET}`,
-      }),
-    });
-
-    const refreshedAccessToken = await response.json();
-    if (response.ok) {
-      return {
-        ...token,
-        accessToken: refreshedAccessToken.access_token,
-        refreshToken: refreshedAccessToken.refresh_token ?? token.refreshToken,
-        accessTokenExpires: Date.now() + refreshedAccessToken.expires_in * 1000,
-      };
-    }
-  } catch (e) {
-    console.error('accessToken을 다시 발급받는 도중 에러', e);
-  }
-};
 
 const handler = NextAuth({
   pages: {
@@ -95,7 +66,7 @@ const handler = NextAuth({
           return token;
         }
       }
-      const newToken = await refreshAccessToken(token);
+      const newToken = await refreshKakaoAccessToken(token);
       return newToken;
     },
     async session({ session, token }) {
