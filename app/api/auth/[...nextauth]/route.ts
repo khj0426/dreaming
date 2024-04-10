a0213df7130e2fa46626c642f2a53299cb9e43ed
MyungJiwoo <1206jiwoo@gmail.com>
2024-04-11 01:07:53 +0900

    [#27] api: 로그인 redirect url 변경 및 로그인한 유저 api 연결

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,88 +1,88 @@
 //app/api/auth/[...nextauth]/route.ts
-import { makeToken, makeRefreshToken } from '../../../lib/token';
+import { makeToken, makeRefreshToken } from "../../../lib/token";
 
-import NextAuth from 'next-auth/next';
-import KakaoProvider from 'next-auth/providers/kakao';
-import { createNewUser } from '../../../lib/user';
-import type { DefaultUser } from 'next-auth';
-import { cookies } from 'next/headers';
+import NextAuth from "next-auth/next";
+import KakaoProvider from "next-auth/providers/kakao";
+import { createNewUser } from "../../../lib/user";
+import type { DefaultUser } from "next-auth";
+import { cookies } from "next/headers";
 
 interface KakaoUser extends DefaultUser {
-  connected_at: string;
-  kakao_account: {
-    profile: {
-      profile_image_url: string;
-      nickname: string;
+    connected_at: string;
+    kakao_account: {
+        profile: {
+            profile_image_url: string;
+            nickname: string;
+        };
+        email: string;
     };
-    email: string;
-  };
 }
 
 const handler = NextAuth({
-  pages: {
-    signIn: '/login',
-  },
-  session: {
-    strategy: 'jwt',
-    //하루 동안 세션 유지
-    maxAge: 24 * 60 * 60,
-  },
-  logger: {},
-  providers: [
-    KakaoProvider({
-      clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID || '',
-      clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET || '',
-      profile(profile, tokens) {
-        return profile;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account, profile, user }) {
-      try {
-        const { kakao_account } = profile as KakaoUser;
-        const refreshToken = makeRefreshToken(user.id);
-        await createNewUser({
-          id: user.id + '',
-          image: kakao_account.profile.profile_image_url,
-          email: kakao_account.email,
-          name: kakao_account.profile.nickname,
-          refreshToken,
-        });
-
-        cookies().set('dreaming_accessToken', makeToken(user.id), {
-          sameSite: 'strict',
-          secure: true,
-          maxAge: 60 * 60 * 1000,
-          httpOnly: true,
-        });
-
-        cookies().set('dreaming_refreshToken', refreshToken, {
-          sameSite: 'strict',
-          secure: true,
-          maxAge: 60 * 60 * 24 * 7 * 1000,
-          httpOnly: true,
-        });
-      } catch (e) {
-        console.error(e);
-      }
-
-      return token;
+    pages: {
+        signIn: "/login",
     },
-    async session({ session, token }) {
-      const sessionUser = {
-        ...token,
-      };
-
-      try {
-        delete sessionUser?.refreshToken;
-      } catch (e) {
-        console.error(e);
-      }
-      session.user = sessionUser;
-      return session;
+    session: {
+        strategy: "jwt",
+        //하루 동안 세션 유지
+        maxAge: 24 * 60 * 60,
     },
-  },
+    logger: {},
+    providers: [
+        KakaoProvider({
+            clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID || "",
+            clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET || "",
+            profile(profile, tokens) {
+                return profile;
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account, profile, user }) {
+            try {
+                const { kakao_account } = profile as KakaoUser;
+                const refreshToken = makeRefreshToken(user.id);
+                await createNewUser({
+                    id: user.id + "",
+                    image: kakao_account.profile.profile_image_url,
+                    email: kakao_account.email,
+                    name: kakao_account.profile.nickname,
+                    refreshToken,
+                });
+
+                cookies().set("dreaming_accessToken", makeToken(user.id), {
+                    sameSite: "strict",
+                    secure: true,
+                    maxAge: 60 * 60 * 1000,
+                    httpOnly: true,
+                });
+
+                cookies().set("dreaming_refreshToken", refreshToken, {
+                    sameSite: "strict",
+                    secure: true,
+                    maxAge: 60 * 60 * 24 * 7 * 1000,
+                    httpOnly: true,
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            return token;
+        },
+        async session({ session, token }) {
+            const sessionUser = {
+                ...token,
+            };
+
+            try {
+                delete sessionUser?.refreshToken;
+            } catch (e) {
+                console.error(e);
+            }
+            session.user = sessionUser;
+            return session;
+        },
+    },
 });
 
 export { handler as GET, handler as POST };
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
-import { cookies } from 'next/headers';
-import { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const jwt = cookies().get('dreaming_accessToken');
+    const jwt = cookies().get("dreaming_accessToken");
 
-  if (!jwt) {
-    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`);
-  }
+    if (!jwt) {
+        return NextResponse.redirect(`${process.env.NEXTAUTH_URL}`);
+    }
 }
 
 //NOTE - 바뀔 필요가 있음.
 export const config = {
-  matcher: ['/main', '/my_diary', '/post', '/lotto', '/lotto_map', '/'],
+    matcher: ["/main", "/my_diary", "/post", "/lotto", "/lotto_map"],
 };
