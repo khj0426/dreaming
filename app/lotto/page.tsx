d621dbf68dc0a7755544c4ca03d819eea3f23af2
MyungJiwoo <1206jiwoo@gmail.com>
2024-04-11 01:42:57 +0900

    [#28] api: 로또 api 연결

--- app/lotto/page.tsx
+++ app/lotto/page.tsx
@@ -5,21 +5,47 @@ import styles from "./lotto.module.css";
 import { GrMoney } from "react-icons/gr";
 import { UserProps } from "../diary/page";
 import { getUser } from "../api/service/user";
+import { getLotto } from "../api/service/lotto";
 
 function LottoPage() {
     const [user, setUser] = useState<UserProps[]>([]);
+    const [lotto, setLotto] = useState<number[]>();
 
     // 애니메이션 활성화 상태
     const [isAnimating, setIsAnimating] = useState(false);
 
     // 버튼 클릭 이벤트 핸들러
     const handlePlayButtonClick = useCallback(() => {
-        setIsAnimating(true);
+        const isConfirmed = window.confirm(
+            "300 포인트를 사용하여 로또 번호를 추천 받으시겠습니까?"
+        );
 
-        // 3초 후 다시 false
-        setTimeout(() => {
-            setIsAnimating(false);
-        }, 3000);
+        if (isConfirmed) {
+            setIsAnimating(true);
+
+            // [api] 랜덤 로또 번호 get
+            setTimeout(() => {
+                setLotto(generateLottoNumbers());
+            }, 2500);
+
+            // (async () => {
+            //     try {
+            //         const data = await getLotto();
+            //         setLotto(data);
+            //         console.log(data);
+            //     } catch (error) {
+            //         console.error(
+            //             "로또 추천 번호를 불러오는 데 실패했습니다.",
+            //             error
+            //         );
+            //     }
+            // })();
+
+            // 3초 후 다시 false
+            setTimeout(() => {
+                setIsAnimating(false);
+            }, 3000);
+        }
     }, []);
 
     // [api] 로그인한 유저 정보 get 요청
@@ -29,13 +55,35 @@ function LottoPage() {
                 const data = await getUser();
                 setUser(data.user);
             } catch (error) {
-                console.error(
-                    "다이어리 목록을 불러오는 데 실패했습니다.",
-                    error
-                );
+                console.error("유저 정보를 불러오는 데 실패했습니다.", error);
             }
         })();
     }, []);
+
+    // [api] 랜덤 로또 번호 get
+    // useEffect(() => {
+    //     (async () => {
+    //         try {
+    //             const data = await getLotto();
+    //             setLotto(data);
+    //         } catch (error) {
+    //             console.error(
+    //                 "로또 추천 번호를 불러오는 데 실패했습니다.",
+    //                 error
+    //             );
+    //         }
+    //     })();
+    // }, []);
+
+    // 로또 번호 7개 추천
+    const generateLottoNumbers = () => {
+        const numbers = new Set<number>();
+        while (numbers.size < 7) {
+            const number = Math.floor(Math.random() * 45) + 1; // 1부터 45까지의 랜덤 숫자
+            numbers.add(number);
+        }
+        return Array.from(numbers);
+    };
 
     return (
         <div className={styles.container}>
@@ -61,25 +109,25 @@ function LottoPage() {
                 }`}
             >
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>1</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[0] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>2</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[1] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>3</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[2] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>4</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[3] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>5</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[4] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>6</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[5] : "?"}</p>
                 </div>
                 <div className={styles.lotto}>
-                    <p className={styles.lottoNum}>7</p>
+                    <p className={styles.lottoNum}>{lotto ? lotto[6] : "?"}</p>
                 </div>
             </div>
 
