eea12de7602e9837f6449a9d07e6ab9f36726152
MyungJiwoo <1206jiwoo@gmail.com>
2024-04-11 17:33:58 +0900

    [#51] api: 통계 api에 차트 추가

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -7,6 +7,10 @@ import { UserProps } from "../diary/page";
 import { getUser } from "../api/service/user";
 import { getLotto } from "../api/service/lotto";
 import { getReport } from "../api/service/report";
+import { Pie } from "react-chartjs-2";
+import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
+
+Chart.register(ArcElement, Tooltip, Legend);
 
 interface ReportProps {
     allCount: number;
@@ -20,30 +24,46 @@ function ReportPage() {
     const [user, setUser] = useState<UserProps | null>(null);
     const [data, setData] = useState<ReportProps | null>(null);
 
-    // [api] 로그인한 유저 정보 get 요청
     useEffect(() => {
         (async () => {
             try {
-                const data = await getUser();
-                setUser(data.user);
+                const user = await getUser(); // [api] 로그인한 유저 정보 get 요청
+                const data = await getReport(); // [api] 통계
+                setUser(user.user);
+                setData(data);
+                // console.log(data.allCount);
             } catch (error) {
                 console.error("유저 정보를 불러오는 데 실패했습니다.", error);
             }
         })();
     }, []);
 
-    // [api] 통계
-    useEffect(() => {
-        (async () => {
-            try {
-                const data = await getReport();
-                console.log(data);
-                setData(data);
-            } catch (error) {
-                console.error("통계 정보를 불러오는 데 실패했습니다.", error);
-            }
-        })();
-    }, []);
+    // 차트
+    const chart = {
+        labels: ["전체 공개 꿈 일기", "나만 보기 꿈 일기"],
+        datasets: [
+            {
+                data: [data?.sharedCount, data?.notSharedCount],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.6)",
+                    "rgba(54, 162, 235, 0.6)",
+                    "rgba(255, 206, 86, 0.6)",
+                    "rgba(75, 192, 192, 0.6)",
+                    "rgba(153, 102, 255, 0.6)",
+                    "rgba(255, 159, 64, 0.6)",
+                ],
+                borderColor: [
+                    "rgba(255,99,132,1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
 
     return (
         <div className={styles.container}>
@@ -51,10 +71,14 @@ function ReportPage() {
             <div className={styles.reportTitleBox}>
                 <p className={styles.reportTitle}>현재 드리밍에는</p>
                 <p className={styles.reportTitle}>
-                    <span className={styles.important_gray}>총 ****개</span>의
-                    글이 있어요!
+                    <span className={styles.important_gray}>
+                        총 {data?.allCount}개
+                    </span>
+                    의 글이 있어요!
                 </p>
-                <div className={styles.chart}></div>
+                <div className={styles.chart}>
+                    <Pie data={chart} style={{ width: "80%", height: "80%" }} />
+                </div>
             </div>
             <div className={styles.reportBox}>
                 <img src="./report_post.png" className={styles.iconImg} />
@@ -67,8 +91,10 @@ function ReportPage() {
                         은
                     </p>
                     <p className={styles.reportText}>
-                        <span className={styles.important_blue}>**</span>개의
-                        글을 작성하셨네요
+                        <span className={styles.important_blue}>
+                            {data?.allCount}
+                        </span>
+                        개의 글을 작성하셨네요
                     </p>
                 </div>
             </div>
@@ -82,8 +108,10 @@ function ReportPage() {
                     </p>
                     <p className={styles.reportText}>
                         좋아요는 총{" "}
-                        <span className={styles.important_blue}>**</span>개
-                        입니다!
+                        <span className={styles.important_blue}>
+                            {data?.likeCount}
+                        </span>
+                        개 입니다!
                     </p>
                 </div>
                 <img src="./report_like.png" className={styles.iconImg} />
@@ -98,8 +126,10 @@ function ReportPage() {
                     </p>
                     <p className={styles.reportText}>
                         댓글은 총{" "}
-                        <span className={styles.important_blue}>**</span>개
-                        입니다!
+                        <span className={styles.important_blue}>
+                            {data?.commentCount}
+                        </span>
+                        개 입니다!
                     </p>
                 </div>
                 <img src="./report_comment.png" className={styles.iconImg} />
